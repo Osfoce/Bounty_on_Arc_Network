59996cbdfa87ad718d8c9741e0d5f2a8505f4a90
osfoce <oselufortunatus@gmail.com>
2026-09-12 15:12:03 +0100

    added creditcoin network

--- frontend/src/rainbowChains.jsx
+++ frontend/src/rainbowChains.jsx
@@ -1,9 +1,9 @@
-import { sepolia, baseSepolia, opBNBTestnet } from "wagmi/chains";
+import { sepolia } from "wagmi/chains";
 
-const injectiveTestnetRpcUrl =
-  "https://k8s.testnet.json-rpc.injective.network/";
-const injectiveTestnetExplorerUrl =
-  "https://testnet.explorer.injective.network/";
+// const injectiveTestnetRpcUrl =
+//   "https://k8s.testnet.json-rpc.injective.network/";
+// const injectiveTestnetExplorerUrl =
+//   "https://testnet.explorer.injective.network/";
 
 // Injective Testnet Configuration
 const injectiveTestnet = {
@@ -11,12 +11,34 @@ const injectiveTestnet = {
   name: "INJ Testnet",
   nativeCurrency: { name: "Injective", symbol: "INJ", decimals: 18 },
   rpcUrls: {
-    default: { http: [injectiveTestnetRpcUrl] },
+    default: { http: ["https://k8s.testnet.json-rpc.injective.network/"] },
   },
   blockExplorers: {
     default: {
       name: "Injective Explorer",
-      url: injectiveTestnetExplorerUrl,
+      url:  "https://testnet.explorer.injective.network/",
+    },
+  },
+  testnet: true,
+};
+
+export const creditcoinTestnet = {
+  id: 102031,
+  name: "Creditcoin Testnet",
+  nativeCurrency: {
+    name: "Creditcoin Testnet",
+    symbol: "tCTC",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.cc3-testnet.creditcoin.network"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Creditcoin Explorer",
+      url: "https://creditcoin-testnet.blockscout.com",
     },
   },
   testnet: true,
@@ -24,7 +46,6 @@ const injectiveTestnet = {
 
 export const supportedChains = [
   sepolia,
-  baseSepolia,
-  opBNBTestnet,
   injectiveTestnet,
+  creditcoinTestnet,
 ];
